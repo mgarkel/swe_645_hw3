8b0806ddc56660fc794591c14f1c9ab26db81923
egecark <ecarkoglu14@ku.edu.tr>
2020-04-16 22:01:14 -0400

    added http client

--- frontend/src/app/survey/survey.component.ts
+++ frontend/src/app/survey/survey.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+<<<<<<< Updated upstream
 import {SubmitService} from '../submit.service';
+=======
+import { HttpClient } from '@angular/common/http'
+>>>>>>> Stashed changes
 
 @Component({
   selector: 'app-survey',
@@ -10,7 +14,7 @@ import {SubmitService} from '../submit.service';
 export class SurveyComponent implements OnInit {
   stringdata: any;
 
-  constructor(private submitservice: SubmitService) { }
+  constructor(private submitservice: SubmitService, private http: HttpClient) { }
 
   ngOnInit(): void {
   }
